88cb7a2a5dec65ef49518362e357f6dd61bf5c86
qudwok_labtop <quddnrquddnr2302@gmail.com>
2022-12-13 12:00:46 +0900

    22.12.13
    오전 공부

---
+++ 예제13-01.js
@@ -0,0 +1,12 @@
+// 스코프 (유효 범위) 란?
+function add(x, y) {
+    // 매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+    // 즉, 매개변수의 스코프 (유효 범위) 는 함수 몸체 내부이다.
+    console.log(x, y); // 2 5
+    return x + y;
+}
+
+add(2, 5);
+
+// 매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+console.log(x, y); // ReferenceError: x is not defined
---
+++ 예제14-05.js
@@ -0,0 +1,12 @@
+var MYAPP = {}; // 전역 네임스페이스 객체
+
+MYAPP.name = 'Lee';
+
+console.log(MYAPP.name);    // 'Lee'
+
+MYAPP.person = {
+    name: 'lee',
+    address: 'seoul'
+};
+
+console.log(MYAPP.person.name);    // lee
